914b6b79713e2df472a0b61906015de9b6a9d3c5
landingpad75 <skydevsmc@outlook.com>
2026-01-13 14:56:02 +0100

    got bored, will continue later

--- src/scenes/game.asm
+++ src/scenes/game.asm
@@ -1,9 +1,33 @@
 [bits 64]
 
+%include "include/raylib.asm"
+%include "include/input.asm"
+
+section .data
+%include "include/colors.asm"
+extern x
+extern y
+
+section .text
 global GameScene_DRAW
 global GameScene_UPDATE
 
-section .text
-
 GameScene_DRAW:
+    mov rcx, [rel x]
+    mov rdx, [rel y]
+    mov r8d, 50
+    mov r9d, 50
+
+    mov rax, [rel RED]
+    mov [rsp + 32], rax
+
+    call DrawRectangle
+    ret
+
+
 GameScene_UPDATE:
+    call mvup
+    call mvdn
+    call mvlt
+    call mvrt
+    ret
